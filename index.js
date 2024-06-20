547a59de5f6b9ee3d5b3da30e9da57b894d989a9
trungnguyenthien <ngthientrung@gmail.com>
2024-06-20 07:03:20 +0700

    .

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import getRawBody from 'raw-body';
 import completionRoute from './routes/completion.js';
+import bodyParser from 'body-parser';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -10,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors());
-
+app.use(bodyParser.json());
 app.use(async (req, res, next) => {
     try {
       req.rawBody = await getRawBody(req, { encoding: 'utf8' });
--- routes/completion.js
+++ routes/completion.js
@@ -19,10 +19,33 @@ router.post('/', async (req, res) => {
   }
 });
 
+
+let clients = {};
+router.get('/events', (req, res) => {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+  const guid = uuidv4().replace(/-/g, '');
+
+  clients[guid] = {
+    guid: guid,
+    res: res
+  };
+
+  // Gửi thông báo khi kết nối SSE được thiết lập
+  res.write(`data: Connected to SSE server with client ID: ${guid}\n\n`);
+
+  req.on('close', () => {
+    console.log(`${clientId} Connection closed`);
+    clients = clients.filter(client => client.id !== clientId);
+  });
+});
+
 router.post('/stream', async (req, res) => {
   try {
-    const message = req.rawBody;
-    await streamWithFunctions(message, res)
+    const { message, clientId } = req.body;
+    const clientRes = clients[clientId].res
+    await streamWithFunctions(message, clientRes)
     // const response = await s(message)
     // res.status(200).json({ status: 200, error: "" });
   } catch (error) {
