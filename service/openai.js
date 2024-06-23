5b1a2c9d39f13be45a92f3bfcca05ebc783957c5
trungnguyenthien <ngthientrung@gmail.com>
2024-06-23 16:14:18 +0700

    refactor

--- service/openai.js
+++ service/openai.js
@@ -34,7 +34,6 @@ export async function requestWithFunctions(requestMessage) {
     tool_choice: "auto", // auto is default, but we'll be explicit
   });
   const responseMessage = response.choices[0].message;
-  console.log(responseMessage)
   // Step 2: check if the model wanted to call a function
   const toolCalls = responseMessage.tool_calls;
   if (toolCalls) {
@@ -61,7 +60,6 @@ export async function requestWithFunctions(requestMessage) {
 }
 
 async function handle_stream_response(response, res) {
-  console.log(`handle_stream_response response`)
   let is_first_chunk = true
   let func_name = ""
   let func_arguments = ""
@@ -70,8 +68,6 @@ async function handle_stream_response(response, res) {
   let tool_calls = {}
 
   for await (const chunk of response) {
-    console.log(`chunk = ${JSON.stringify(chunk)}`)
-
     if (!(chunk.choices[0] && chunk.choices[0].delta)) {
       continue
     }
@@ -103,7 +99,6 @@ async function handle_stream_response(response, res) {
       res.write(sse_message(content))
     } else {
       func_arguments += delta.tool_calls[0].function.arguments
-      console.log(`finish_reason = ${JSON.stringify(func_arguments)}`)
     }
   }
 
@@ -141,7 +136,6 @@ export async function streamWithFunctions2(requestMessage, res) {
   let finish_reason = null
   finish_reason = await handle_stream_response(response, res)
   do {
-    console.log(`enter do...while() .... ${JSON.stringify(finish_reason)}`)
     if (finish_reason.function) {
       const functionName = finish_reason.function
       const functionToCall = availableFunctions[functionName];
@@ -167,69 +161,16 @@ export async function streamWithFunctions2(requestMessage, res) {
         stream: true,
         tool_choice: "auto",
       });
-      console.log(`send next message = ${JSON.stringify(messages)}`)
       finish_reason = await handle_stream_response(response, res)
     }
   } while (finish_reason !== "stop")
-
-  // while(isToolCall(response)) {
-  //   const responseMessage = response.choices[0].message;
-  //   const toolCalls = responseMessage.tool_calls;
-  //   if (toolCalls) {
-  //     messages.push(responseMessage); // extend conversation with assistant's reply
-  //     for (const toolCall of toolCalls) {
-  //       const functionName = toolCall.function.name;
-  //       const functionToCall = availableFunctions[functionName];
-  //       const functionArgs = JSON.parse(toolCall.function.arguments);
-  //       const functionResponse = await functionToCall(functionArgs);
-
-  //       messages.push({
-  //         tool_call_id: toolCall.id,
-  //         role: "tool",
-  //         name: functionName,
-  //         content: JSON.stringify(functionResponse),
-  //       }); // extend conversation with function response
-  //     }
-  //     response = await openai.chat.completions.create({
-  //       model: "gpt-4o",
-  //       messages: messages,
-  //       tools: tools,
-  //       stream: true,
-  //       tool_choice: "auto", 
-  //     });
-  //   }
-  // }
-
-  // for await (const chunk of response) {
-  //   const [choice] = chunk.choices;
-  //   const { content } = choice.delta;
-  //   res.write(sse_message(content))
-  // }
-}
-
-function isStopResponse(response) {
-  return response.choices.finish_reason === 'stop'
-
-}
-
-function isToolCall(response) {
-  const responseMessage = response.choices[0].message;
-  // const toolCalls = responseMessage.tool_calls;
-  if (responseMessage.tool_calls) {
-    return true;
-  }
-  return false
 }
 
 function sse_message(data) {
-  // console.log(`stream: ${data}`)
   return `data: ${textToBase64(data)}\n\n`
 }
 
 function textToBase64(text) {
-  // Chuyển đổi text thành Buffer
   const buffer = Buffer.from(text, 'utf-8');
-
-  // Chuyển đổi Buffer thành chuỗi base64
   return buffer.toString('base64');
 }
